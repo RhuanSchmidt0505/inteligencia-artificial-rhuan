06bfdade8c368757fb9d2ec43bb13eff61352860
RhuanSchmidt0505 <104856922+RhuanSchmidt0505@users.noreply.github.com>
2024-07-26 10:36:51 -0300

    .

---
+++ main.js
@@ -0,0 +1,28 @@
+const perguntas = [
+    pergunta1{
+        enunciado:"Você gosta de bola?",
+        alternativas [
+             {
+                texto: "sim"
+                afirmacao: "Você é uma pessoa normal"
+            } 
+            {
+                texto:"não"
+                afirmacao: "você é uma pessoa bem estranho"
+            }]
+
+    }
+    pergunta2{
+        enunciado:"Você dorme?",
+        alternativas [
+             {
+                texto: "sim"
+                afirmacao: "Você é uma pessoa normal"
+            } 
+            {
+                texto:"não"
+                afirmacao: "você é uma pessoa bem estranho"
+            }]
+
+    }
+]
